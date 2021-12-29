648e70c80e19788568865dd27f58420aeb6c4288
Anirudh Iyer <anirudh.iyer@outlook.com>
2021-12-29 15:39:00 -0800

    Add project files.

---
+++ Model/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+    }
+}
